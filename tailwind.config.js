fe9f33f9585922f397b49f5096dab25ef1f71497
Hannah Oliveros <email>
2023-03-28 14:09:52 +0800

    added js

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,8 +24,8 @@ module.exports = {
         padding: '1rem',
         screens: {
           sm: '600px',
-          md: '728px',
-          lg: '984px',
+          md: '768px',
+          lg: '900px',
           xl: '1240px',
         //  '2xl': '1496px',
         },
